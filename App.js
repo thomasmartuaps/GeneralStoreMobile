c90d0746df42ec89d69eedd61d01aa92e3b3ea2f
thomasmartuaps <thomasmartuaps@gmail.com>
2020-07-21 10:13:59 +0700

    wanna go grab lunch

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './src/store'
+
+import NavigationPage from './src/views/NavigationPage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <NavigationPage></NavigationPage>
+    </Provider>
   );
 }
 
