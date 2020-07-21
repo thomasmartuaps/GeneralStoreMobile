d20188095aa99ee66f1302dbc64a7b7c528eca3c
thomasmartuaps <thomasmartuaps@gmail.com>
2020-07-21 20:19:23 +0700

    working on it slowly

--- src/views/NavigationPage.js
+++ src/views/NavigationPage.js
@@ -1,1 +1,17 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator()
+
+export default function NavigationPage () {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" headerMode="none">
+        <Stack.Screen name="Login"></Stack.Screen>
+        <Stack.Screen name="StorePage"></Stack.Screen>
+        <Stack.Screen name="ProfilePage"></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
